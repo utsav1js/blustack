e171f9b4a40d27c1274f3d6c70d7229278e2f6ce
utsav <utsav@197NODMB21072.local>
2019-11-27 06:21:24 +0530

    Updates

---
+++ precache-manifest.60aea9e5e95236ff8a7ea6144cad6ab3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10f7b8b07e8ef8771b5c41398f5598e",
+    "url": "/blustack/index.html"
+  },
+  {
+    "revision": "9ad44a77b7fd7f4cba8a",
+    "url": "/blustack/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "f46af7289bbce1f31e84",
+    "url": "/blustack/static/css/main.f9c2f316.chunk.css"
+  },
+  {
+    "revision": "9ad44a77b7fd7f4cba8a",
+    "url": "/blustack/static/js/2.ab425523.chunk.js"
+  },
+  {
+    "revision": "f46af7289bbce1f31e84",
+    "url": "/blustack/static/js/main.c01b5a8e.chunk.js"
+  },
+  {
+    "revision": "83d0983060ecccd780f4",
+    "url": "/blustack/static/js/runtime-main.ae0a3c7c.js"
+  },
+  {
+    "revision": "30b942c323034b911bfcbc6e33c1fb68",
+    "url": "/blustack/static/media/80.30b942c3.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/blustack/static/media/camp1.977ecf7b.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/blustack/static/media/camp3.3ecfc5bd.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/blustack/static/media/camp4.8a4d68db.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/blustack/static/media/camp5.c161ba4a.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/blustack/static/media/camp6.e51f42c3.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/blustack/static/media/camp7.90640a87.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/blustack/static/media/camp8.7173cfe7.png"
+  },
+  {
+    "revision": "aff6dfc6a1b059d995aee89227bc9431",
+    "url": "/blustack/static/media/logo.aff6dfc6.png"
+  }
+]);
